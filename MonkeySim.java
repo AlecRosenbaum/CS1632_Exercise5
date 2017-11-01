28f5b357653ea8a91f081dec3b472d43445d9368
Alec Rosenbaum <alecr95@gmail.com>
2017-11-01 10:18:08 -0400

    fixed some more functions

--- MonkeySim.java
+++ MonkeySim.java
@@ -75,22 +75,15 @@ public class MonkeySim {
      */
 
     public static String stringifyResults(int c, Monkey m, Monkey m2) {
-        String toReturn = new String("");
         try {
-            for (int j = 0; j < HEADER; j++) {
-                toReturn += "@";
-            }
-            toReturn += new String("//Round ");
-            toReturn += new String("" + c);
-            toReturn += new String(": Threw banana from Monkey (#");
-            toReturn += new String(m.getMonkeyNum() + " / ID " + m.getId());
-            toReturn += new String(") to Monkey (#");
-            toReturn += new String(m2.getMonkeyNum() + " / ID " + m2.getId() + ")");
+             return "//Round " + c + ": " + 
+               "Threw banana from Monkey (#" + m.getMonkeyNum() + " / ID " + m.getId() + ")" +
+               " to Monkey (#" + m2.getMonkeyNum() + " / ID " + m2.getId() + ")";
         } catch (NoIdException noidex) {
             System.out.println("INVALID MONKEY!");
             System.exit(2);
         }
-        return toReturn.substring(HEADER);
+        return null;
     }
 
     /**
@@ -143,7 +136,9 @@ public class MonkeySim {
     public static int runSimulation(List<Monkey> ml, MonkeyWatcher mw) {
         int nextMonkey = -1;
 
-        while (!getFirstMonkey(ml).hasBanana()) {
+        Monkey first = getFirstMonkey(ml);
+
+        while (!first.hasBanana()) {
             mw.incrementRounds();
             Monkey m = ml.get(monkeyWithBanana(ml));
             int n = nextMonkeyAndResize(m, ml);
